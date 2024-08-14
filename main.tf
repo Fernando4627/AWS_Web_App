df2c9da9aa2af107884bfe14c40f2816051fc195
Eric Gibbons <39319408+eric994499@users.noreply.github.com>
2024-08-14 10:09:09 -0400

    network changes

--- main.tf
+++ main.tf
@@ -44,6 +44,16 @@ resource "aws_subnet" "app_vpc_subnet" {
   }
 }
 
+resource "aws_network_interface" "app_network" {
+  subnet_id   = aws_subnet.app_vpc_subnet.id
+  private_ips = ["10.0.1.100","10.0.1.101"]
+
+  tags = {
+    Name = "primary_network_interface"
+    Env = "TestTerraAWS"
+  }
+}
+
 resource "aws_ecs_cluster" "app_clust" {
   name = "app-cluster"
   tags = {
@@ -56,6 +66,8 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   count=2
   user_data = filebase64("scripts/user_data.sh")
+  subnet_id = aws_subnet.app_vpc_subnet.id
+  network_interface = aws_network_interface.app_network.id
   vpc_security_group_ids= [aws_security_group.allow_http.id]
   tags = {
     Name = element(var.awsl_name_list, count.index)
@@ -70,7 +82,6 @@ resource "aws_s3_bucket" "app_storage" {
     Env  = "TestTerraAWS"
   }
 }
-
 
 
 resource "aws_resourcegroups_group" "test" {
