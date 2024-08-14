2afd3fed16d6e7a099fbb4afbe58aca53a032839
Eric Gibbons <39319408+eric994499@users.noreply.github.com>
2024-08-14 09:41:23 -0400

    terra web ui sucks

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
-
 data "aws_vpc" "app_vpc" {
   cidr_block = "172.31.0.0/16"
   tags = {
