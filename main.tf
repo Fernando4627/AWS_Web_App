f0d920b6baf1034fdb03891db373e41118091292
Eric Gibbons <39319408+eric994499@users.noreply.github.com>
2024-08-14 09:40:03 -0400

    last test

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+
 data "aws_vpc" "app_vpc" {
   cidr_block = "172.31.0.0/16"
   tags = {
