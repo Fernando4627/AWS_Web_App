10908afc7c87ef203ec2ca50c477aa16d89aa270
Eric Gibbons <39319408+eric994499@users.noreply.github.com>
2024-08-14 11:10:56 -0400

    ssh

--- main.tf
+++ main.tf
@@ -34,6 +34,31 @@ resource "aws_security_group" "allow_http" {
     Env = "TestTerraAWS"
   }
 }
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+    Env = "TestTerraAWS"
+  }
+}
 
 resource "aws_subnet" "app_vpc_subnet" {
   vpc_id     = aws_vpc.app_vpc.id
