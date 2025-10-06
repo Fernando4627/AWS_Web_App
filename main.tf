d38c83575e15e799579ae3da908b7d52288cc807
Eric Gibbons <39319408+eric994499@users.noreply.github.com>
2025-10-06 14:58:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ resource "aws_ecs_cluster" "app_clust" {
 resource "aws_instance" "app_server" {
   ami = var.aws_linux
   instance_type = "t2.micro"
-  count=2
+  count=3
   user_data = file("./scripts/user_data.sh")
   user_data_replace_on_change = true
   subnet_id = aws_subnet.app_vpc_subnet.id
@@ -130,3 +130,4 @@ resource "aws_resourcegroups_group" "test" {
 JSON
   }
 }
+
